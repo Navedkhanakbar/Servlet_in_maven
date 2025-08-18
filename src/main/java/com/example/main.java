69699c9d22115ff57595d1caf3f72ac36fd112e6
Naved Khan <nk9167755768@gmail.com>
2025-08-18 21:40:02 +0530

    updated main java file.

--- src/main/java/com/example/main.java
+++ src/main/java/com/example/main.java
@@ -1,4 +1,20 @@
 package com.example;
 
-public class main {
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/hello")
+public class main extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response )
+            throws IOException, ServletException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello from servlet in Maven!</h1>");
+
+    }
 }
